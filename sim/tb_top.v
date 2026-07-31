5354ed6abb1ade2237a3cd7261e9b7b9ae49aa07
Sailors111 <421607938@qq.com>
2026-07-31 16:49:01 +0800

    Update docs, RTL, hex and simulation files

--- sim/tb_top.v
+++ sim/tb_top.v
@@ -59,11 +59,17 @@ module tb_top;
         $dumpvars(0, tb_top);
     end
 
-    reg [31:0] clock = -32'd1;
+    integer cycle;
+
+    initial begin
+        cycle = -1;
+    end
 
     always @(posedge clk) begin
+        cycle = cycle + 1;
+        #1;
         $display("------------------------------------------------------------");
-        $display("clock=C%0d", $signed(clock));
+        $display("cycle=C%0d", cycle);
         $display("time=%0t", $time);
         $display("IF   : pc=%h instr=%h", if_pc_out, if_instr_out);
         $display("IFID : pc=%h instr=%h", if_id_pc_out, if_id_instr_out);
@@ -71,25 +77,26 @@ module tb_top;
         $display("EXMEM: alu_out=%h", ex_mem_alu_result_out);
         $display("MEMWB: alu_out=%h mem_out=%h", mem_wb_alu_result_out, mem_wb_mem_data_out);
         $display("WB   : rd=%0d we=%b wdata=%h", wb_rd_out, wb_reg_write_out, wb_wdata_out);
-        clock <= clock + 32'd1;
+        $display("REG  : x0=%h x1=%h x2=%h x3=%h",
+                 u_top.u_id.u_regfile.regs[0],
+                 u_top.u_id.u_regfile.regs[1],
+                 u_top.u_id.u_regfile.regs[2],
+                 u_top.u_id.u_regfile.regs[3]);
+        $display("REG  : x4=%h x5=%h x6=%h x7=%h",
+                 u_top.u_id.u_regfile.regs[4],
+                 u_top.u_id.u_regfile.regs[5],
+                 u_top.u_id.u_regfile.regs[6],
+                 u_top.u_id.u_regfile.regs[7]);
+        $display("REG  : x8=%h x9=%h x10=%h x11=%h",
+                 u_top.u_id.u_regfile.regs[8],
+                 u_top.u_id.u_regfile.regs[9],
+                 u_top.u_id.u_regfile.regs[10],
+                 u_top.u_id.u_regfile.regs[11]);
+        $display("DMEM : mem[0]=%h mem[1]=%h mem[2]=%h mem[3]=%h",
+                 u_top.u_mem.u_data_ram.mem[0],
+                 u_top.u_mem.u_data_ram.mem[1],
+                 u_top.u_mem.u_data_ram.mem[2],
+                 u_top.u_mem.u_data_ram.mem[3]);
     end
-
-    // initial begin
-    //     #1;
-    //     $display("time   x0       x1       x2       x3       x4       x5       x6       x7");
-    //     forever begin
-    //         #10;
-    //         $display("%4t  %h %h %h %h %h %h %h %h",
-    //                  $time,
-    //                  u_top.u_id.u_regfile.regs[0],
-    //                  u_top.u_id.u_regfile.regs[1],
-    //                  u_top.u_id.u_regfile.regs[2],
-    //                  u_top.u_id.u_regfile.regs[3],
-    //                  u_top.u_id.u_regfile.regs[4],
-    //                  u_top.u_id.u_regfile.regs[5],
-    //                  u_top.u_id.u_regfile.regs[6],
-    //                  u_top.u_id.u_regfile.regs[7]);
-    //     end
-    // end
 
 endmodule
